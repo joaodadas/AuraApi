dea33b019924802f83aabfee6138fe211f4480c2
Joao Vitor <joao@cargon.com.br>
2024-03-04 14:23:06 -0300

    feat: add GetUsersController

--- src/controller/get-users/get-users.ts
+++ src/controller/get-users/get-users.ts
@@ -1,4 +1,19 @@
-import { IGetUsersController } from "./protocols";
+import { IGetUsersController, IGetUsersRepository } from "./protocols";
 export class GetUsersController implements IGetUsersController {
-  handle() {}
+  constructor(private readonly getUsersRepository: IGetUsersRepository) {}
+
+  async handle() {
+    try {
+      const users = await this.getUsersRepository.getUsers();
+      return {
+        statusCode: 200,
+        body: users,
+      };
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: "Something went wrong",
+      };
+    }
+  }
 }
--- src/controller/get-users/protocols.ts
+++ src/controller/get-users/protocols.ts
@@ -1,3 +1,11 @@
-interface IGetUsersController {
-  handle(): any;
+import User from "../../models/user"
+import { HttpResponse } from "../protocols";
+export interface IGetUsersController {
+  handle(): Promise<HttpResponse<User[]>>;
 }
+
+export interface IGetUsersRepository {
+  getUsers(): Promise<User[]>;
+} 
+
+export interface 
