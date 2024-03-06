d8092dc228182c3944e68b7e685225749db78b2d
Joao Vitor <joao@cargon.com.br>
2024-03-06 14:01:44 -0300

    feat: add id to User

--- src/repositories/get-users/mongo-get-users.ts
+++ src/repositories/get-users/mongo-get-users.ts
@@ -1,8 +1,13 @@
 import { IGetUsersRepository } from "@/controller/get-users/protocols";
+import { MongoClient } from "@/db/mongo";
 import { User } from "@/models/user";
 
 export class MongoGetUsersRepository implements IGetUsersRepository {
   async getUsers(): Promise<User[]> {
+    const users = await MongoClient.db
+      .collection<User>("users")
+      .find({})
+      .toArray();
     return [
       {
         firstName: "João Vitor",
